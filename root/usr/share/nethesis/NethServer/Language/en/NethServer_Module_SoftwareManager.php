351ccea862bab33cf4e806356d948d9bddb1e8c7
Davide Principi <davide.principi@nethesis.it>
2012-10-24 13:03:59 +0200

    SoftwareManager cleanup..

--- root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_SoftwareManager.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- */
-
-$L['SoftwareManager_Title'] = 'Software Manager';
-$L['Register_label'] = 'Register';
-$L['NethServer\Module\SoftwareManager_caption'] = 'Software Manager';
-$L['SystemID_label'] = 'License Key';
-$L['Additional_Software_label'] = 'Additional Software Management';
