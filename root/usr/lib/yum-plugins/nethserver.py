798f4fb8c61634a14c7a458f7c4d5b6bb41d9b5d
Davide Principi <davide.principi@nethesis.it>
2012-10-31 12:45:12 +0100

    Fixed event-queue dependency in nethserver yum plugin. Refs #1502

--- root/usr/lib/yum-plugins/nethserver.py
+++ root/usr/lib/yum-plugins/nethserver.py
@@ -1,24 +1,29 @@
 from subprocess import call
 from yum.plugins import TYPE_CORE
+import os.path
 
 requires_api_version = '2.1'
 plugin_type = (TYPE_CORE,)
+EVENT_QUEUE_CMD = '/sbin/e-smith/event-queue'
 
 def pretrans_hook(conduit):
     if conduit.confBool("main", "verbose", default=0): #if verbose
         conduit.info(2, "Enabling NethServer queue")
-    call("/sbin/e-smith/event-queue enable", shell=True)
+    if hasEventCommand():
+        call(EVENT_QUEUE_CMD + " enable", shell=True)
 
 def posttrans_hook(conduit):
     if conduit.confBool("main", "verbose", default=0): #if verbose
         conduit.info(2, "Executing NethServer queue")
-    call("/sbin/e-smith/event-queue signal", shell=True)
+    if hasEventCommand():
+        call(EVENT_QUEUE_CMD + " signal", shell=True)
 
 def close_hook(conduit):
-    call("/sbin/e-smith/event-queue enable", shell=True)
     if conduit.confBool("main", "verbose", default=0): #if verbose
         conduit.info(2, "Flushing NethServer queue")
-    # clean up queue
-    call("/sbin/e-smith/event-queue disable", shell=True)
+    if hasEventCommand():
+        # clean up queue
+        call(EVENT_QUEUE_CMD + " disable", shell=True) 
 
-
+def hasEventCommand():
+    return os.path.isfile(EVENT_QUEUE_CMD)
