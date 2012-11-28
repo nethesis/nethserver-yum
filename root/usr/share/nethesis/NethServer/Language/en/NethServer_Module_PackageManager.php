9492c54fa58b77aba2948cea9160ef5ddfa5fcd5
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2012-11-28 18:18:35 +0100

    Translation: refactor english and italian translation files #523

--- root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_PackageManager.php
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_PackageManager.php
@@ -1,17 +1,17 @@
-<?php
+<?php 
 
-$L['PackageManager_Title'] = 'Package manager';
-$L['PackageManager_Description'] = 'Adds and removes system package groups';
-$L['PackageManager_Tags'] = 'rpm yum addons extras';
-$L['Name_label'] = 'Group name';
-$L['Status_label'] = 'Status';
-$L['Add_label'] = 'Add';
-$L['Remove_label'] = 'Remove';
-$L['SelectedOptionalPackages_label'] = 'Optional packages';
-$L['Add ${0}'] = 'Add ${0}';
-$L['Remove ${0}'] = 'Remove ${0}';
-$L['available'] = 'available';
-$L['installed'] = 'installed';
-$L['installing'] = 'installing..';
-$L['removing'] = 'removing..';
-$L['mandatory_packages'] = 'Mandatory packages:';
+$L['Add ${0}'] = 'Add ${0}'; 
+$L['Add_label'] = 'Add'; 
+$L['Name_label'] = 'Group name'; 
+$L['PackageManager_Description'] = 'Adds and removes system package groups'; 
+$L['PackageManager_Tags'] = 'rpm yum addons extras'; 
+$L['PackageManager_Title'] = 'Package manager'; 
+$L['Remove ${0}'] = 'Remove ${0}'; 
+$L['Remove_label'] = 'Remove'; 
+$L['SelectedOptionalPackages_label'] = 'Optional packages'; 
+$L['Status_label'] = 'Status'; 
+$L['available'] = 'available'; 
+$L['installed'] = 'installed'; 
+$L['installing'] = 'installing..'; 
+$L['mandatory_packages'] = 'Mandatory packages:'; 
+$L['removing'] = 'removing..'; 
