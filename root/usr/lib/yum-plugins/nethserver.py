35a1bda4e651f1d2753a87d47b33572aff79334e
Davide Principi <davide.principi@nethesis.it>
2013-05-06 15:44:10 +0200

    nethserver.py (posttrans_hook): ensure package list is sorted respecting RPM dependencies. Refs #1870

--- root/usr/lib/yum-plugins/nethserver.py
+++ root/usr/lib/yum-plugins/nethserver.py
@@ -24,52 +24,72 @@ import os.path
 import os
 import rpm
 
-def insertBefore(list, el, search):
-    i = 0
-    k = -1
-    for l in list:
-        if l == search:
-            k = i
-        i=i+1
-    if k < 0:
-        list.append(el)
-    else:
-        list.insert(k,el)
-    return list
+requires_api_version = '2.1'
+plugin_type = (TYPE_CORE,)
 
-def uniq(list):
-    seen = set()
-    seen_add = seen.add
-    return [ x for x in list if x not in seen and not seen_add(x)]
+signal_event = '/sbin/e-smith/signal-event'
+events_dir = '/etc/e-smith/events'
 
-def postUpgrade():
-    list = ['nethserver-lib']
+def read_package_list():
+    """
+    Query RPM db for any nethserver* package and return the list of
+    package names, preserving the dependency order. RPM 'Require' tags
+    are parsed.
+    """
+
+    list = []
     ts = rpm.TransactionSet()
     mi = ts.dbMatch()
     for h in mi:
-        if not h['name'].startswith('nethserver'): continue
-        for dep in h[rpm.RPMTAG_REQUIRENAME]:
-            if not dep.startswith('nethserver'): continue
-            insertBefore(list,dep,h['name']);
-        list.append(h['name'])
+        if not h['name'].startswith('nethserver'): 
+            continue
 
-    for p in uniq(list):
+        for dep in h[rpm.RPMTAG_REQUIRENAME]:
+            if not dep.startswith('nethserver'): 
+                continue
+
+            try:
+                # insert deps before the package name
+                list.insert(list.index(h['name']), dep)
+            except ValueError:
+                list.append(dep)
+
+        if not h['name'] in list:
+            list.append(h['name'])
+
+    # Reduce the given list preserving the element order
+    s = set()
+    return filter(lambda x: x not in s and not s.add(x) , list) 
+
+
+def reconfigure_all_packages():
+    """Signal *-update event for any nethserver* package"""
+
+    for p in read_package_list():
         event = "%s-update" % p
         if os.path.isdir('/etc/e-smith/events/%s' % event):
             os.spawnl(os.P_WAIT, signal_event, signal_event, event)
 
 
-requires_api_version = '2.1'
-plugin_type = (TYPE_CORE,)
-signal_event = '/sbin/e-smith/signal-event'
+def adjust_all_services():
+    """Signal firewall-adjust and runlevel-adjust system events"""
+
+    if os.path.isdir(events_dir + "/firewall-adjust"):
+        os.spawnl(os.P_WAIT, signal_event, signal_event, "firewall-adjust")
+    if os.path.isdir(events_dir + "/runlevel-adjust"):
+        os.spawnl(os.P_WAIT, signal_event, signal_event, "runlevel-adjust")
+
 
 
 def posttrans_hook(conduit):
+    """ The yum post-RPM-transaction hook """
+
     installed = []
     erased = []
-    isUninstall = False
+
     if conduit.confBool("main", "verbose", default=0): #if verbose
         conduit.info(2, "Executing NethServer queue")
+
     ts = conduit.getTsInfo()
 
     for tsmem in ts.getMembers():
@@ -77,23 +97,21 @@ def posttrans_hook(conduit):
             if tsmem.ts_state == 'i' or tsmem.ts_state == 'u':
                 installed.append(tsmem.name)
             elif tsmem.ts_state == 'e':
-                isUninstall = True
                 erased.append(tsmem.name)
 
-    installed.reverse()
+    # Get the list of installed/update packages respecting the
+    # dependency sorting:
+    installed = filter(lambda x: x in installed, read_package_list())
+    
     for ipkg in installed:
         event = "%s-update" % ipkg
-        if os.path.isdir("/etc/e-smith/events/%s" % event):
+        if os.path.isdir(events_dir + "/%s" % event):
             if conduit.confBool("main", "verbose", default=0): #if verbose
                 conduit.info(2, "Executing signal-event %s" % event)
             os.spawnl(os.P_WAIT, signal_event, signal_event, event)
 
-    if isUninstall:
-        postUpgrade()
+    if len(erased) > 0:
+        reconfigure_all_packages()
 
-    if len(installed) > 0:
-        if os.path.isdir("/etc/e-smith/events/firewall-adjust"):
-            os.spawnl(os.P_WAIT, signal_event, signal_event, "firewall-adjust")
-        if os.path.isdir("/etc/e-smith/events/runlevel-adjust"):
-            os.spawnl(os.P_WAIT, signal_event, signal_event, "runlevel-adjust")
-
+    if len(installed) > 0 or len(erased) > 0:
+        adjust_all_services()
